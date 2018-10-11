dcbb7b0feb25ccf68e35628cdd3d641e1302b282
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-10 17:58:13 -0700

    Amazing Rings

--- src/modulo/Amazingrings.java
+++ src/modulo/Amazingrings.java
@@ -1,33 +1,48 @@
-package modulo;
-
-public class Amazingrings {
-	void setup() {
-		size(500, 500);
-	}
-
-	void draw() {
-
-		int x = 10;
-		int y = 10;
-
-		for (int i = 0; i < 15; i++) {
-			noFill();
-			x += 10;
-			y += 10;
-			ellipse(100, 250, x, y);
-
-		}
-	}
-
-	int x = 10;
-	int y=10;
-
-	for(
-	int i = 0;i<15;i++)
-	{
-		noFill();
-		x += 10;
-		y += 10;
-		ellipse(400, 250, x, y);
-	}
+int w;
+int y;
+int h;
+int w2;
+int h2;
+int x;
+int x2;
+void setup() {
+    size(800, 500);
+x=100; 
+x2=600;
 }
+
+  void draw() {
+background(#E3E0E0);
+w=10;
+h=10;
+
+    for (int i = 0; i < 15; i++) {
+      noFill();
+      w +=10;
+      h +=10;
+      ellipse(x, 250,w, h);
+      
+    
+  }
+  x=x+1;
+  if(x>600){
+    x=100;
+    x=x-1;
+    
+  }
+  w2=10;
+  h2=10;
+  for (int i = 0; i < 15; i++) {
+      noFill();
+      w2 +=10;
+      h2 +=10;
+      ellipse(x2, 250, w2, h2);
+}
+x2=x2-1;
+
+if(x2<100){
+ x2=600;
+ x2=x2+1;
+}
+  }
+ 
